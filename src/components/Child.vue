143e15a1ad60cd69be3d732773b036be0c6e53cf
Abhay Mehta <abhayrockks@gmail.com>
2020-07-13 19:23:00 +0530

    second commit

---
+++ src/components/Child.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="id">
+    <button @click="$emit(togglelable)">{{label}}</button>
+    <button @click="$emit(callback)">AlertBox</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "child",
+  props: {
+    label: String
+  }
+  //   methods: {
+  //     callback: function(e) {
+  //       this.$emit("click", e);
+  //     },
+
+  //     toggleLable: function(e) {
+  //       this.$emit("click", e);
+  //     }
+  //   }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Parent.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <child v-on:togglelable="toggleLable($event)" v-on:callback="callback($event)" :label.sync="label"></child>
+
+</template>
+
+<script>
+import child from "./Child.vue";
+export default {
+  name: "Button",
+  components: {
+    child
+  },
+  data() {
+    return {
+      label: "Primary"
+    };
+  },
+
+  methods: {
+    toggleLable: function() {
+      if (this.label == "Primary") {
+        this.label = this.label.split("") .reverse().join("");
+      } else {
+        this.label = "Primary";
+      }
+    },
+    callback: function() {
+      alert("Hi");
+    }
+  }
+};
+</script>
+
+<style>
+button {
+  padding: 10px 20px;
+  border: 1px solid #ddd;
+  color: rgb(240, 221, 221);
+  background-color: rgb(49, 103, 204);
+  border-radius: 4px;
+  font-size: 14px;
+  cursor: pointer;
+}
+</style>
